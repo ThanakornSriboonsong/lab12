8a6beb6e04c03e8768d4f6246778265310fc6241
Pleum <plemplemplemplem5@gmail.com>
2024-12-24 15:30:58 +0700

    done

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,16 +1,8 @@
 #include<iostream>
-<<<<<<< HEAD
 #include<ctime>
 using namespace std;
 
 int fibonacci(int x ) ;
-=======
-#include <ctime>
-using namespace std;
-
-
-long long int fibonacci(long long int x ) ;
->>>>>>> 8d2d191fb7a7079085984e115c573f4ce1f65317
 
 int main(){
     clock_t start = clock();
@@ -19,13 +11,10 @@ int main(){
     double elapsed = double(end - start)/CLOCKS_PER_SEC;
     cout << "Elapsed Time: " << elapsed << " seconds.";
     return 0;
+    
 }
 
-<<<<<<< HEAD
 int fibonacci(int x ){
-=======
-long long int fibonacci(long long int x ){
->>>>>>> 8d2d191fb7a7079085984e115c573f4ce1f65317
     if(x > 0){
     if( x == 0 ) x = 0 ;
     else if( x == 1 ) x = 1 ;
